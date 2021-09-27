e0bf30577c5dcd28a48a1c070c8c45423d1bc1cd
Hamza Ghelle <hamzaghellegmail.com>
2021-09-27 16:21:43 -0400

    add routes for urls_shows and urls_index

--- express_server.js
+++ express_server.js
@@ -2,12 +2,23 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
-app.set("view engine", "ejs");
-
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.set("view engine", "ejs");
+
+app.get("/urls", (req, res) =>{
+  const templateVars = {urls: urlDatabase}
+  res.render("urls_index", templateVars);
+});
+
+app.get( "/urls/:shortURL", (req, res) => {
+  const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL}
+  res.render("urls_shows", templateVars)
+});
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
