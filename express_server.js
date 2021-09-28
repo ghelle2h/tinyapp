0acc250ed01632f4faef38cabd8e7dc92d9d95d2
Hamza Ghelle <hamzaghellegmail.com>
2021-09-27 21:42:50 -0400

    Add get and post for /urls/new and function to generate random string

--- express_server.js
+++ express_server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+function generateRandomString() {
+  return Math.random().toString(20).substr(2, 6);
+}
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -12,6 +16,15 @@ app.set("view engine", "ejs");
 app.get("/urls", (req, res) =>{
   const templateVars = {urls: urlDatabase}
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get( "/urls/:shortURL", (req, res) => {
